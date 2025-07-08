96a32f2f7e29288b497f2c233da551af86f15577
Lea Tinoso <lxthym@icloud.com>
2025-07-08 22:09:26 +0800

    feat: fix clerk middleware position

--- src/server.ts
+++ src/server.ts
@@ -34,14 +34,14 @@ app.use(
   })
 );
 
+app.use(clerkMiddleware());
+
 // Routes
 app.use('/api', routes);
 
 // Error handling middleware
 app.use(notFound);
 app.use(errorHandler);
-
-app.use(clerkMiddleware());
 
 const PORT = config.port;
 
